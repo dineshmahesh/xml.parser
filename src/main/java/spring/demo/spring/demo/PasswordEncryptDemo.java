06b685c0ae8994f22779180e43f904be1d638d4d
Dinesh Mahesh <dineshmaheshusha@gmail.com>
2019-12-06 01:55:57 +0530

    password encryption and decription added

--- src/main/java/spring/demo/spring/demo/PasswordEncryptDemo.java
+++ src/main/java/spring/demo/spring/demo/PasswordEncryptDemo.java
@@ -1,8 +1,78 @@
 package spring.demo.spring.demo;
 
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+
 public class PasswordEncryptDemo {
 	
-	public static void main(String[] args) {
-//		encrypt(strClearText, strKey)
-	}
+	private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+    private static final String myEncryptionKey = "ThisIsSpartaThisIsSparta";
+    private static KeySpec keySpec;
+    private static SecretKeyFactory secretKeyFactory;
+    private static Cipher cipher;
+    static  byte[] arrayBytes;
+    static SecretKey key;
+    
+    static {
+    	try {
+			init();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    public static void init() throws Exception {
+        arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+        keySpec = new DESedeKeySpec(arrayBytes);
+        secretKeyFactory = SecretKeyFactory.getInstance(DESEDE_ENCRYPTION_SCHEME);
+        cipher = Cipher.getInstance(DESEDE_ENCRYPTION_SCHEME);
+        key = secretKeyFactory.generateSecret(keySpec);
+    }
+
+
+    public static String encrypt(String unencryptedString) {
+        String encryptedString = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = Base64.getEncoder().encodeToString(encryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+
+
+    public static String decrypt(String encryptedString) {
+        String decryptedText=null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.getDecoder().decode(encryptedString);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText = new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
+
+
+    public static void main(String args []) throws Exception
+    {
+        String target="password";
+        String encrypted = encrypt(target);
+        String decrypted = decrypt(encrypted);
+
+        System.out.println("String To Encrypt: "+ target);
+        System.out.println("Encrypted String:" + encrypted);
+        System.out.println("Decrypted String:" + decrypted);
+
+    }
 }
--- src/main/java/spring/demo/spring/demo/PasswordUtils.java
+++ src/main/java/spring/demo/spring/demo/PasswordUtils.java
@@ -5,54 +5,54 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 
 public class PasswordUtils {
 
 	private static Cipher cipher;
-
-	public PasswordUtils() throws NoSuchAlgorithmException, NoSuchPaddingException {
-		cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	private static SecretKey secretKey;
+	static {
+		try {
+			init();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void init() throws NoSuchAlgorithmException, NoSuchPaddingException {
+		cipher = Cipher.getInstance("DESede");
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("DESede");
+		keyGenerator.init(168);
+		secretKey = keyGenerator.generateKey();
 	}
 
-	private static String encode(String password) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException {
-		char[] chars = password.toCharArray();
-        byte[] salt = getSalt();
-         
-        PBEKeySpec spec = new PBEKeySpec(chars, salt, 1000, 256);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        SecretKey secretKey = factory.generateSecret(spec);
+	private static String encode(String password) throws NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		byte[] passwordBytes = password.getBytes();
+
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] hash = factory.generateSecret(spec).getEncoded();
-        return new String(hash);
+        byte[] encodedPassword = cipher.doFinal(passwordBytes);
+        return new String(encodedPassword);
     }
 
-	private static String decode(String encodedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException {
-		char[] chars = encodedPassword.toCharArray();
-		byte[] salt = getSalt();
-		
-		PBEKeySpec spec = new PBEKeySpec(chars, salt, 1000, 256);
-		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-		SecretKey secretKey = factory.generateSecret(spec);
+	private static String decode(String encodedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		byte[] chars = encodedPassword.getBytes();
+
 		cipher.init(Cipher.DECRYPT_MODE, secretKey);
-		byte[] hash = factory.generateSecret(spec).getEncoded();
-		return new String(hash);
+		cipher.doFinal(chars);
+		return new String(chars);
 	}
 	
-	private static byte[] getSalt() throws NoSuchAlgorithmException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return salt;
-    }
-	
-	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException {
-		String encodeed = encode("password");
-		System.out.println(encodeed);
+	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		String encoded = PasswordUtils.encode("password");
+		String decoded = PasswordUtils.decode(encoded);
+		System.out.println(encoded);
+		System.out.println(decoded);
 	}
 	
 }
